10023be728c36075795f2153be5f68d19ed13135
vrubezhny <vrubezhny@exadel.com>
2015-02-27 22:53:21 +0300

    Bug 461086 - Sonar: Correctness - Impossible cast
    Issue is fixed
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/core/dom/ASTMatcher.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/core/dom/ASTMatcher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -450,7 +450,7 @@ public class ASTMatcher {
 	}
 
 	public boolean match(RegularExpressionLiteral node, Object other) {
-		if (!(other instanceof CharacterLiteral)) {
+		if (!(other instanceof RegularExpressionLiteral)) {
 			return false;
 		}
 		RegularExpressionLiteral o = (RegularExpressionLiteral) other;
