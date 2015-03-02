f6bd2c69ce4a8036a52ddcee3bacc9f65ac8f285
vrubezhny <vrubezhny@exadel.com>
2015-03-02 05:40:02 +0300

    Bug 433515 - Eclipse freezes every few minutes
    Issue is fixed
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/lookup/CompilationUnitScope.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/lookup/CompilationUnitScope.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -674,6 +674,7 @@ public class CompilationUnitScope extends BlockScope {
 						//only add if field with name that is not already added
 						if(!superBindingFieldsNames.includes(fieldsOnGlobalField[fieldsOnGlobalFieldIndex].name)) {
 							superBindingFields.add(fieldsOnGlobalField[fieldsOnGlobalFieldIndex]);
+							superBindingFieldsNames.add(fieldsOnGlobalField[fieldsOnGlobalFieldIndex].name);
 						}
 					}
 				}
