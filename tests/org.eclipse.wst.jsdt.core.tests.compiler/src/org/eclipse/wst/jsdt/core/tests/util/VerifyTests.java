3c6033435228798a6e5696000dc79284109e8096
vrubezhny <vrubezhny@exadel.com>
2015-03-28 03:38:42 +0300

    Bug 463334 - Sonar : Method may fail to clean up stream or resource on checked exception
    Issue is fixed
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- tests/org.eclipse.wst.jsdt.core.tests.compiler/src/org/eclipse/wst/jsdt/core/tests/util/VerifyTests.java
+++ tests/org.eclipse.wst.jsdt.core.tests.compiler/src/org/eclipse/wst/jsdt/core/tests/util/VerifyTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -132,11 +132,12 @@ public class VerifyClassLoader extends ClassLoader {
 			try {
 				byte[] b= new byte[stream.available()];
 				stream.read(b);
-				stream.close();
 				return b;
 			}
 			catch (IOException e) {
 				throw new ClassNotFoundException();
+			} finally {
+				try { stream.close(); } catch (IOException e) { /* Ignore */ }
 			}
 		}
 		catch (FileNotFoundException e) {
