2181fa57d8238b9381a1e9b0249f72c51c6e8773
vrubezhny <vrubezhny@exadel.com>
2015-06-30 19:26:08 +0300

    Bug 466258 - NullPointerException in DiskIndex.mergeCategory (481)
    Issue is fixed
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/core/index/DiskIndex.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/core/index/DiskIndex.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -478,7 +478,9 @@ private void mergeCategory(char[] categoryName, DiskIndex onDisk, int[] position
 				}
 			}
 		}
-		onDisk.categoryTables.put(categoryName, null); // flush cached table
+		if (onDisk.categoryTables != null) {
+			onDisk.categoryTables.put(categoryName, null); // flush cached table
+		}
 	}
 	writeCategoryTable(categoryName, wordsToDocs, stream);
 }
