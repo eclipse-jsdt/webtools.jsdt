f1dde662e3ff5cfe97aa2236fc798ebc38ed5028
vrubezhny <vrubezhny@exadel.com>
2015-07-21 14:42:26 +0300

    Bug 473175 - NullPointerException in BindingLabelProvider.getAdornmentFlags (42)
    Issue is fixed
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/viewsupport/BindingLabelProvider.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/viewsupport/BindingLabelProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -33,9 +33,11 @@ import org.eclipse.wst.jsdt.ui.JavaScriptElementLabels;
  * 
  */
 public class BindingLabelProvider extends LabelProvider {
-	
 
 	private static int getAdornmentFlags(IBinding binding, int flags) {
+		if (binding == null)
+			return 0;
+		
 		int adornments= 0;
 		if (binding instanceof IFunctionBinding && ((IFunctionBinding) binding).isConstructor())
 			adornments|= JavaScriptElementImageDescriptor.CONSTRUCTOR;
