3a36e53c4e0b37779911e353cd701a997e29e139
vrubezhny <vrubezhny@exadel.com>
2015-07-27 16:38:46 +0300

    Bug 473658 - IllegalArgumentException in InternalJob.schedule (439)
    Issue is fixed
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/core/index/PostponedRunnablesManager.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/core/index/PostponedRunnablesManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
+ * Copyright (c) 2011, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -198,7 +198,8 @@ class PostponedRunnablesManager {
 							runnable.run();
 						} else {
 							long timeToWait = runnable.timeToWait();
-							shortestTimeToWait = shortestTimeToWait <  timeToWait? shortestTimeToWait : timeToWait;
+							timeToWait = timeToWait < 0 ? 0 : timeToWait;
+							shortestTimeToWait = shortestTimeToWait < timeToWait? shortestTimeToWait : timeToWait;
 							this.addAction(runnable);
 						}
 					}
