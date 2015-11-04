dcb268a5313845226a2632f2a4387ecc0d6b0d24
vrubezhny <vrubezhny@exadel.com>
2015-11-04 22:37:33 +0300

    Bug 481447 - ArithmeticException in CommentFormattingStrategy.inferIndentationLevel (222)
    Issue is fixed
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/text/comment/CommentFormattingStrategy.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/text/comment/CommentFormattingStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -219,9 +219,12 @@ public class CommentFormattingStrategy extends ContextBasedFormattingStrategy {
 		if (tabSize == 0)
 			return referenceWidth;
 
-		int level= referenceWidth / indentSize;
-		if (referenceWidth % indentSize > 0)
-			level++;
+		int level= referenceWidth;
+		if (indentSize > 0) {
+			level= referenceWidth/ indentSize;
+			if (referenceWidth % indentSize > 0)
+				level++;
+		}
 		return level;
 	}
 
