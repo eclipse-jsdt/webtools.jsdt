7bf2befa64a873f028fadc9793667e794ecd4f9e
Ian Stewart-Binks <istewart@redhat.com>
2016-03-15 16:53:41 -0400

    Bug 489861 - [cleanup] Clean up TemplateCompletionProposalComputer.java
    Change-Id: I787a46f5eca52187ddb2a81f78ce3e6b459e4fdd
    Signed-off-by: Ian Stewart-Binks <istewart@redhat.com>

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/text/java/TemplateCompletionProposalComputer.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/text/java/TemplateCompletionProposalComputer.java
@@ -19,6 +19,7 @@ import java.util.List;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.TextUtilities;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.templates.TemplateContextType;
 import org.eclipse.wst.jsdt.core.IJavaScriptUnit;
 import org.eclipse.wst.jsdt.internal.corext.template.java.JavaContextType;
@@ -33,106 +34,106 @@ import org.eclipse.wst.jsdt.ui.text.java.IJavaCompletionProposal;
 import org.eclipse.wst.jsdt.ui.text.java.IJavaCompletionProposalComputer;
 import org.eclipse.wst.jsdt.ui.text.java.JavaContentAssistInvocationContext;
 
-/**
- * 
- * 
- */
 public final class TemplateCompletionProposalComputer implements IJavaCompletionProposalComputer {
-	
+
 	private final TemplateEngine fJavaTemplateEngine;
 	private final TemplateEngine fJavadocTemplateEngine;
 
 	public TemplateCompletionProposalComputer() {
-		TemplateContextType contextType= JavaScriptPlugin.getDefault().getTemplateContextRegistry().getContextType(JavaContextType.NAME);
+		TemplateContextType contextType = JavaScriptPlugin.getDefault().getTemplateContextRegistry().getContextType(JavaContextType.NAME);
+
 		if (contextType == null) {
-			contextType= new JavaContextType();
+			contextType = new JavaContextType();
 			JavaScriptPlugin.getDefault().getTemplateContextRegistry().addContextType(contextType);
 		}
-		if (contextType != null)
-			fJavaTemplateEngine= new TemplateEngine(contextType);
-		else
-			fJavaTemplateEngine= null;
-		contextType= JavaScriptPlugin.getDefault().getTemplateContextRegistry().getContextType("javadoc"); //$NON-NLS-1$
+
+		fJavaTemplateEngine = new TemplateEngine(contextType);
+		contextType = JavaScriptPlugin.getDefault().getTemplateContextRegistry().getContextType("javadoc"); //$NON-NLS-1$
+
 		if (contextType == null) {
-			contextType= new JavaDocContextType();
+			contextType = new JavaDocContextType();
 			JavaScriptPlugin.getDefault().getTemplateContextRegistry().addContextType(contextType);
 		}
-		if (contextType != null)
-			fJavadocTemplateEngine= new TemplateEngine(contextType);
-		else
-			fJavadocTemplateEngine= null;
+
+		fJavadocTemplateEngine = new TemplateEngine(contextType);
 	}
-	
+
 	/*
 	 * @see org.eclipse.jface.text.contentassist.ICompletionProposalComputer#computeCompletionProposals(org.eclipse.jface.text.contentassist.TextContentAssistInvocationContext, org.eclipse.core.runtime.IProgressMonitor)
 	 */
-	public List computeCompletionProposals(ContentAssistInvocationContext context, IProgressMonitor monitor) {
+	public List<ICompletionProposal> computeCompletionProposals(ContentAssistInvocationContext context, IProgressMonitor monitor) {
 		TemplateEngine engine;
 		try {
-			String partition= TextUtilities.getContentType(context.getDocument(), IJavaScriptPartitions.JAVA_PARTITIONING, context.getInvocationOffset(), true);
-			if (partition.equals(IJavaScriptPartitions.JAVA_DOC))
-				engine= fJavadocTemplateEngine;
-			else
-				engine= fJavaTemplateEngine;
+			String partition = TextUtilities.getContentType(context.getDocument(), IJavaScriptPartitions.JAVA_PARTITIONING, context.getInvocationOffset(), true);
+			if (partition.equals(IJavaScriptPartitions.JAVA_DOC)) {
+				engine = fJavadocTemplateEngine;
+			} else {
+				engine = fJavaTemplateEngine;
+			}
 		} catch (BadLocationException x) {
-			return Collections.EMPTY_LIST;
+			return Collections.emptyList();
 		}
-		
-		if (engine != null) {
-			if (!(context instanceof JavaContentAssistInvocationContext))
-				return Collections.EMPTY_LIST;
 
-			JavaContentAssistInvocationContext javaContext= (JavaContentAssistInvocationContext) context;
-			IJavaScriptUnit unit= javaContext.getCompilationUnit();
-			if (unit == null)
-				return Collections.EMPTY_LIST;
-			
+		if (engine != null) {
+			if (!(context instanceof JavaContentAssistInvocationContext)) {
+				return Collections.emptyList();
+			}
+
+			JavaContentAssistInvocationContext javaContext = (JavaContentAssistInvocationContext) context;
+			IJavaScriptUnit unit = javaContext.getCompilationUnit();
+			if (unit == null) {
+				return Collections.emptyList();
+			}
+
 			engine.reset();
 			engine.complete(javaContext.getViewer(), javaContext.getInvocationOffset(), unit);
 
-			TemplateProposal[] templateProposals= engine.getResults();
-			List result= new ArrayList(Arrays.asList(templateProposals));
+			TemplateProposal[] templateProposals = engine.getResults();
+			List<ICompletionProposal> result = new ArrayList<ICompletionProposal>(Arrays.asList(templateProposals));
 
-			IJavaCompletionProposal[] keyWordResults= javaContext.getKeywordProposals();
+			IJavaCompletionProposal[] keyWordResults = javaContext.getKeywordProposals();
 			if (keyWordResults.length > 0) {
-				List removals= new ArrayList();
-				
+				List<TemplateProposal> removals = new ArrayList<TemplateProposal>();
+
 				// update relevance of template proposals that match with a keyword
 				// give those templates slightly more relevance than the keyword to
 				// sort them first
 				// remove keyword templates that don't have an equivalent
 				// keyword proposal
 				if (keyWordResults.length > 0) {
-					outer: for (int k= 0; k < templateProposals.length; k++) {
-						TemplateProposal curr= templateProposals[k];
-						String name= curr.getTemplate().getName();
-						for (int i= 0; i < keyWordResults.length; i++) {
-							String keyword= keyWordResults[i].getDisplayString();
+					outer: for (int k = 0; k < templateProposals.length; k++) {
+						TemplateProposal curr = templateProposals[k];
+						String name = curr.getTemplate().getName();
+
+						for (int i = 0; i < keyWordResults.length; i++) {
+							String keyword = keyWordResults[i].getDisplayString();
 							if (name.startsWith(keyword)) {
 								curr.setRelevance(keyWordResults[i].getRelevance() + 1);
 								continue outer;
 							}
 						}
-						if (isKeyword(name))
+
+						if (isKeyword(name)) {
 							removals.add(curr);
+						}
 					}
 				}
-				
+
 				result.removeAll(removals);
 			}
 			return result;
 		}
-		
-		return Collections.EMPTY_LIST;
+
+		return Collections.emptyList();
 	}
 
 	/*
 	 * @see org.eclipse.jface.text.contentassist.ICompletionProposalComputer#computeContextInformation(org.eclipse.jface.text.contentassist.TextContentAssistInvocationContext, org.eclipse.core.runtime.IProgressMonitor)
 	 */
-	public List computeContextInformation(ContentAssistInvocationContext context, IProgressMonitor monitor) {
-		return Collections.EMPTY_LIST;
+	public List<ICompletionProposal> computeContextInformation(ContentAssistInvocationContext context, IProgressMonitor monitor) {
+		return Collections.emptyList();
 	}
-	
+
 	private boolean isKeyword(String name) {
 		return KeywordUtilities.getInstance().isKeyword(name);
 	}
@@ -157,4 +158,5 @@ public final class TemplateCompletionProposalComputer implements IJavaCompletion
 		fJavadocTemplateEngine.reset();
 		fJavaTemplateEngine.reset();
 	}
+
 }
