d0105ba849e23c19aaa70a2974d338cc6193d792
Victor Rubezhny <vrubezhny@exadel.com>
2016-07-14 01:25:06 +0300

    Bug 497873 - NullPointerException in CreateMultipleSourceFoldersDialog.open
    Signed-off-by: Victor Rubezhny <vrubezhny@exadel.com>

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/wizards/buildpaths/CreateMultipleSourceFoldersDialog.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/wizards/buildpaths/CreateMultipleSourceFoldersDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -176,7 +176,7 @@ public class CreateMultipleSourceFoldersDialog extends TrayDialog {
 
 		if (dialog.open() == Window.OK) {
 			Object[] elements= dialog.getResult();	
-			for (int i= 0; i < elements.length; i++) {
+			for (int i= 0; elements != null && i < elements.length; i++) {
 				IResource res= (IResource)elements[i];
 				
 				IPath[] defaultExclusions = JavaScriptCore.getJavaScriptCore().getDefaultClasspathExclusionPatterns(); 
