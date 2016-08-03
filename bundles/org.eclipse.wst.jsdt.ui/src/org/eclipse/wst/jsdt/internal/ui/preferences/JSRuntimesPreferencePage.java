35b3e87fa27e5bbe8ba75a092c7c144d7829346d
Shane Bryzak <sbryzak@redhat.com>
2016-08-04 08:32:24 +1000

    Bug 486065 Chromium Browser detection for Windows - Fixed array index out of bounds issues in JSRuntimesPreferencePage
    Change-Id: I776b8d3073430974782e7e34fc16ba864e8a861e
    Signed-off-by: Shane Bryzak <sbryzak@redhat.com>

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/preferences/JSRuntimesPreferencePage.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/ui/preferences/JSRuntimesPreferencePage.java
@@ -54,31 +54,32 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 	 * ID for the page
 	 */
 	public static final String ID = "org.eclipse.wst.jsdt.ui.preferences.RuntimesPreferencePage"; //$NON-NLS-1$
-	
+
 	private String[] runtimeNamesArray;
 	private String[] runtimeIndexToIdsMap;
-	
-	private Map <String, IJSRuntimeInstall> defaultInstallByType; 
+
+	private Map <String, IJSRuntimeInstall> defaultInstallByType;
 	private Map <String, IJSRuntimeInstall[]> fRuntimeInstallsByType;
 	private Map <String, String> fOriginalRuntimesTimeStamp;
 	private Map <String, String> fLastRuntimesTimeStamp;
-	
+
 	private String currentRuntimeTypeId;
-	
+
 	private Combo fJSTypeCombo;
 	private InstalledJSRuntimesBlock fJSRuntimesBlock;
-	
+
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
 	 */
+	@Override
 	public void init(IWorkbench workbench) {
 		defaultInstallByType = new HashMap<String, IJSRuntimeInstall> ();
 		fRuntimeInstallsByType = new HashMap <String, IJSRuntimeInstall[]>();
 		fOriginalRuntimesTimeStamp = new HashMap <String, String> ();
 		fLastRuntimesTimeStamp = new HashMap <String, String> ();
 	}
-	
+
 	private void initRuntimeTypes () {
 		// We don't know anything about the runtime types, so let's gather
 		// the information we need.
@@ -86,30 +87,31 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 			Collection <IJSRuntimeType> runtimeTypes = JSRuntimeManager.getJSRuntimeTypes();
 			IJSRuntimeType[] jsRuntimeTypes = new IJSRuntimeType[runtimeTypes.size()];
 			runtimeTypes.toArray(jsRuntimeTypes);
-			
+
 			// Sort the elements for display... just once
 			Arrays.sort(jsRuntimeTypes, new Comparator<IJSRuntimeType>() {
+				@Override
 				public int compare(IJSRuntimeType runtime1, IJSRuntimeType runtime2) {
 					return runtime1.getName().compareToIgnoreCase(runtime2.getName());
 				}
 			});
-			
+
 			runtimeIndexToIdsMap = new String[jsRuntimeTypes.length];
 			runtimeNamesArray = new String[jsRuntimeTypes.length];
-			
+
 			for (int i = 0; i < jsRuntimeTypes.length; ++i) {
 				// This is the list of names that will be shown in our combo.
 				// We can safely use this here because we know they are already
 				// ordered
 				runtimeNamesArray[i] = jsRuntimeTypes[i].getName();
-				
+
 				// Build a map from index (in the array, which is the same will be used in the combo)
 				// to ids, which is the correct and easiest way to handle the runtime types.
 				runtimeIndexToIdsMap[i] = jsRuntimeTypes[i].getId();
 			}
 		}
 	}
-	
+
 	private void initDefaultRuntimeInstallsMap () {
 		defaultInstallByType.clear();
 		Collection <String> runtimeTypeIds = JSRuntimeManager.getJSRuntimeTypesIds();
@@ -118,25 +120,25 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 			defaultInstallByType.put(runtimeTypeId, runtimeInstall);
 		}
 	}
-	
+
 	private void initRuntimeInstallsByTypeMap () {
 		fRuntimeInstallsByType.clear();
 		Collection <String> runtimeTypeIds = JSRuntimeManager.getJSRuntimeTypesIds();
 		for (String runtimeTypeId : runtimeTypeIds) {
-			IJSRuntimeInstall[] runtimeInstalls = 
+			IJSRuntimeInstall[] runtimeInstalls =
 						JSRuntimeManager.getJSRuntimeInstallsByType(runtimeTypeId);
 			fRuntimeInstallsByType.put(runtimeTypeId, runtimeInstalls);
 		}
 	}
-	
+
 	private String getRuntimeTypeId (int selection) {
-		if (selection != -1) {
+		if (selection >= 0 && selection < runtimeIndexToIdsMap.length) {
 			return runtimeIndexToIdsMap[selection];
 		}
-		
+
 		return null;
 	}
-	
+
 	private void backupTimeStamp () {
 		String oldTimeStamp = fOriginalRuntimesTimeStamp.get (currentRuntimeTypeId);
 		if (oldTimeStamp == null) {
@@ -145,42 +147,46 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 		}
 		fLastRuntimesTimeStamp.put(currentRuntimeTypeId, fJSRuntimesBlock.getTimeStamp());
 	}
-	
+
 	private void clearEnvironment () {
 		initRuntimeInstallsByTypeMap ();
 		initDefaultRuntimeInstallsMap();
 		fOriginalRuntimesTimeStamp.clear();
 		fLastRuntimesTimeStamp.clear();
-		fJSTypeCombo.select(0);
 		currentRuntimeTypeId = getRuntimeTypeId(0);
-		fJSRuntimesBlock.setRuntimeType (JSRuntimeManager.getJSRuntimeType(currentRuntimeTypeId));
-		initDefaultRuntime ();
+		if (currentRuntimeTypeId != null) {
+			fJSTypeCombo.select(0);
+			fJSRuntimesBlock.setRuntimeType(JSRuntimeManager.getJSRuntimeType(currentRuntimeTypeId));
+			initDefaultRuntime();
+		}
 		backupTimeStamp();
 	}
 
+	@Override
 	protected Control createContents(Composite ancestor) {
 		initializeDialogUnits(ancestor);
-		
+
 		noDefaultButton();
-		
+
 		GridLayout layout= new GridLayout();
 		layout.numColumns= 1;
 		layout.marginHeight = 0;
 		layout.marginWidth = 0;
 		ancestor.setLayout(layout);
-		
+
 		SWTFactory.createWrapLabel(ancestor, PreferencesMessages.JSRuntimesPreferencePage_JSRuntimes_Title, 1, 300);
 		SWTFactory.createVerticalSpacer(ancestor, 1);
-		
+
 		// TODO: Test with empty combo (i.e. no runtime types)
 		initRuntimeTypes();
-		
+
 		fJSTypeCombo = SWTFactory.createCombo(ancestor, SWT.BORDER | SWT.READ_ONLY, 1, runtimeNamesArray);
 		fJSTypeCombo.addSelectionListener(new SelectionAdapter() {
 
 			/* (non-Javadoc)
 			 * @see org.eclipse.swt.events.SelectionAdapter#widgetSelected(org.eclipse.swt.events.SelectionEvent)
 			 */
+			@Override
 			public void widgetSelected(SelectionEvent e) {
 				// When combo changes, backup the current runtime installs because those are about
 				// to be lost, then make sure to re-populate the runtime installs list with the
@@ -190,25 +196,25 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 				backupTimeStamp ();
 				currentRuntimeTypeId = getRuntimeTypeId (fJSTypeCombo.getSelectionIndex());
 				// Inform the runtimes block that the type has change so major updates will happen
-				fJSRuntimesBlock.setRuntimeType (JSRuntimeManager.getJSRuntimeType(currentRuntimeTypeId), 
+				fJSRuntimesBlock.setRuntimeType (JSRuntimeManager.getJSRuntimeType(currentRuntimeTypeId),
 							fRuntimeInstallsByType.get(currentRuntimeTypeId));
 				updateDefaultRuntimeInstall (defaultInstallByType.get(currentRuntimeTypeId));
 				// Backup timestamp for the new runtime type we are about to work with
 				backupTimeStamp();
 				super.widgetSelected(e);
 			}
-			
+
 		});
-		
+
 		fJSRuntimesBlock = new InstalledJSRuntimesBlock();
 		fJSRuntimesBlock.createControl(ancestor);
 		Control control = fJSRuntimesBlock.getControl();
 		GridData data = new GridData(GridData.FILL_BOTH);
 		data.horizontalSpan = 1;
 		control.setLayoutData(data);
-		
+
 		fJSRuntimesBlock.restoreColumnSettings(JavaScriptPlugin.getDefault().getDialogSettings(), ID);
-		
+
 		fJSRuntimesBlock.addSelectionChangedListener(new ISelectionChangedListener() {
 			@Override
 			public void selectionChanged(SelectionChangedEvent event) {
@@ -222,7 +228,7 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 						setErrorMessage(PreferencesMessages.JSRuntimesPreferencePage_NoRuntimes_Error);
 					}
 					else {
-						setErrorMessage(PreferencesMessages.JSRuntimesPreferencePage_NoDefaultRuntime_Error); 
+						setErrorMessage(PreferencesMessages.JSRuntimesPreferencePage_NoDefaultRuntime_Error);
 					}
 				} else {
 					setMessage(null);
@@ -233,14 +239,14 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 			}
 		});
 		applyDialogFont(ancestor);
-		
+
 		clearEnvironment();
-		
+
 		return ancestor;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private void initDefaultRuntime() {
 		IJSRuntimeInstall realDefault= JSRuntimeManager.getDefaultRuntimeInstall(currentRuntimeTypeId);
@@ -266,6 +272,7 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 	/* (non-Javadoc)
 	 * @see org.eclipse.jface.preference.PreferencePage#performOk()
 	 */
+	@Override
 	public boolean performOk() {
 		final boolean[] canceled = new boolean[] {false};
 		BusyIndicator.showWhile(null, new Runnable() {
@@ -273,7 +280,7 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 			public void run() {
 				// Backup current block state
 				fRuntimeInstallsByType.put(currentRuntimeTypeId, fJSRuntimesBlock.getRuntimeInstalls());
-				
+
 				Collection<String> runtimeTypesIds = fRuntimeInstallsByType.keySet();
 				for (String runtimeTypeId : runtimeTypesIds) {
 					IJSRuntimeInstall [] currentInstalls = fRuntimeInstallsByType.get(runtimeTypeId);
@@ -285,8 +292,8 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 						}
 						currentInstallIds.add(currentInstalls[i].getId());
 					}
-					
-					// Now, need to iterate the other way around to identify 
+
+					// Now, need to iterate the other way around to identify
 					// removed entries
 					IJSRuntimeInstall [] allInstalls = JSRuntimeManager.getJSRuntimeInstallsByType(runtimeTypeId);
 					for (int i = 0; i < allInstalls.length; ++i) {
@@ -295,38 +302,38 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 							JSRuntimeManager.removeJSRuntimeInstall(allInstalls[i].getId());
 						}
 					}
-					
+
 					IJSRuntimeInstall defaultInstall = defaultInstallByType.get(runtimeTypeId);
 					// Update default install but just if it exists
 					if (defaultInstall != null) {
 						JSRuntimeManager.setDefaultRuntimeInstall(runtimeTypeId, defaultInstall.getId());
 					}
 				}
-				
+
 				// Reset timestamps
 				fOriginalRuntimesTimeStamp.clear();
 				fLastRuntimesTimeStamp.clear();
-				
+
 				try {
 					JSRuntimeManager.saveRuntimesConfiguration ();
 				}
 				catch (CoreException e) {
 					Logger.log(Logger.ERROR, "Error saving preferences for JavaScript runtimes.", e); //$NON-NLS-1$
-				}	
+				}
 			}
 		});
-		
+
 		if(canceled[0]) {
 			return false;
 		}
-		
+
 		// save column widths
 		IDialogSettings settings = JavaScriptPlugin.getDefault().getDialogSettings();
 		fJSRuntimesBlock.saveColumnSettings(settings, ID);
-				
+
 		return super.performOk();
 	}
-	
+
 	/*
 	 * @see org.eclipse.jface.preference.PreferencePage#okToLeave()
 	 */
@@ -335,7 +342,7 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 		// Backup current block state so you can confidently iterate over
 		// updated collections;
 		backupTimeStamp ();
-		
+
 		boolean hasChanges = false;
 		Set <String> keys = fOriginalRuntimesTimeStamp.keySet();
 		for (String key : keys) {
@@ -346,15 +353,15 @@ public class JSRuntimesPreferencePage extends PreferencePage implements IWorkben
 				break;
 			}
 		}
-		
+
 		if (hasChanges) {
 			String title = PreferencesMessages.JSRuntimesPreferencePage_JSRuntimes_Title;
 			String message = PreferencesMessages.JSRuntimesPreferencePage_UnsavedChanges_Message;
 			// Just give options to apply or discard which simplifies the code and
 			// allows us to reuse when switching the runtime types combo box selection.
-			String[] buttonLabels = new String[] { 
-						PreferencesMessages.JSRuntimesPreferencePage_ApplyButton_Label, 
-						PreferencesMessages.JSRuntimesPreferencePage_DiscardButton_Label, 
+			String[] buttonLabels = new String[] {
+						PreferencesMessages.JSRuntimesPreferencePage_ApplyButton_Label,
+						PreferencesMessages.JSRuntimesPreferencePage_DiscardButton_Label,
 						PreferencesMessages.JSRuntimesPreferencePage_ApplyLaterButton_Label };
 			MessageDialog dialog = new MessageDialog(getShell(), title, null, message, MessageDialog.QUESTION, buttonLabels, 0);
 			int res = dialog.open();
