df900ab2b882606385c5a51f14057773010fcf88
Shane Bryzak <sbryzak@redhat.com>
2016-08-10 10:19:01 +1000

    Bug 498453 Automatic Chrome / Chromium Detection in JavaScript Runtimes, added detection for Chrome in Linux
    Change-Id: I7540f4c1060acf7538b8b5e2e5afda1a0ac46a81
    Signed-off-by: Shane Bryzak <sbryzak@redhat.com>

--- bundles/org.eclipse.wst.jsdt.chromium.debug.js/src/org/eclipse/wst/jsdt/chromium/debug/js/util/ChromiumDetector.java
+++ bundles/org.eclipse.wst.jsdt.chromium.debug.js/src/org/eclipse/wst/jsdt/chromium/debug/js/util/ChromiumDetector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Red Hat, Inc. 
+ * Copyright (c) 2016 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -33,6 +33,7 @@ public final class ChromiumDetector {
 	public static final String CHROMIUM_EXTRA_LOCATION = "/usr/local/bin"; //$NON-NLS-1$
 	private static final String CHROMIUM_BROWSER = "chromium-browser"; //$NON-NLS-1$
 	private static final String CHROME_WINDOWS = "chrome.exe"; //$NON-NLS-1$
+	private static final String CHROME_LINUX = "google-chrome"; //$NON-NLS-1$
 	private static final String CHROME_WINDOWS_REG_HKCU_LOCATION = "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\App Paths\\chrome.exe"; //$NON-NLS-1$
 	private static final String CHROME_WINDOWS_REG_HKLM_LOCATION = "HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\App Paths\\chrome.exe"; //$NON-NLS-1$
 	private static final String CHROME_WINDOWS_REG_KEY_NAME = "Path"; //$NON-NLS-1$
@@ -48,39 +49,15 @@ public final class ChromiumDetector {
 	 *         any was found, or null otherwise
 	 */
 	public static File findChromiumSystemPath() {
-		// TODO in future browsers from General -> Web Browser preferences should be used 
+		// TODO in future browsers from General -> Web Browser preferences should be used
 		// TODO need to test for all OS - tested only on Ubuntu 16.04 / only chromium-browser is processed correctly
 		File chromiumFile = null;
 		boolean isWindows = true;
-		List<String> cmdLine = new ArrayList<String>();
 
-		// Is windows?
-		if (Platform.getOS().startsWith("win")) { //$NON-NLS-1$
-			// Handle command in a different way
-			cmdLine.add("cmd"); //$NON-NLS-1$
-			cmdLine.add("/c"); //$NON-NLS-1$
-			cmdLine.add(WHERE_COMMAND);
-		} else {
-			// Assume all other OS behave as *nix systems.
-			isWindows = false;
-			cmdLine.add(WHICH_LOCATION_1);
-		}
+		String output = locateSystemCommandPath(CHROMIUM_BROWSER);
 
-		cmdLine.add(CHROMIUM_BROWSER);
-
-		String output = executeCmd(cmdLine);
-
-		if (!isWindows) {
-			if (output.equals(EMPTY)) {
-				cmdLine.remove(0);
-				cmdLine.add(0, WHICH_LOCATION_2);
-				output = executeCmd(cmdLine);
-				if (output.equals(EMPTY)) {
-					cmdLine.remove(0);
-					cmdLine.add(0, WHICH_LOCATION_3);
-					output = executeCmd(cmdLine);
-				}
-			}
+		if (output.equals(EMPTY)) {
+			output = locateSystemCommandPath(CHROME_LINUX);
 		}
 
 		if (output.equals(EMPTY)) {
@@ -109,16 +86,16 @@ public final class ChromiumDetector {
 					chromiumFile = tempFile;
 				}
 			}
-			
+
 			if (chromiumFile == null && isWindows) {
-				// We will try to determine the location of Chrome from the Windows registry, checking both 
-				// HKEY_CURRENT_USER (for user-only Chrome installation) and HKEY_LOCAL_MACHINE (for all-users 
+				// We will try to determine the location of Chrome from the Windows registry, checking both
+				// HKEY_CURRENT_USER (for user-only Chrome installation) and HKEY_LOCAL_MACHINE (for all-users
 				// Chrome installation).
 				path = WindowsRegistryUtil.readKeyValue(CHROME_WINDOWS_REG_HKCU_LOCATION, CHROME_WINDOWS_REG_KEY_NAME);
 				if (path == null) {
 					path = WindowsRegistryUtil.readKeyValue(CHROME_WINDOWS_REG_HKLM_LOCATION, CHROME_WINDOWS_REG_KEY_NAME);
 				}
-				
+
 				if (path != null) {
 					File tempFile = new File(path, chromeFileName);
 					if (tempFile.exists()) {
@@ -126,16 +103,52 @@ public final class ChromiumDetector {
 						chromiumFile = tempFile;
 					}
 				}
-			}			
+			}
 			return chromiumFile;
 		}
 		chromiumFile = new File(output);
 		return chromiumFile;
 	}
 
+	private static String locateSystemCommandPath(String commandName) {
+
+		List<String> cmdLine = new ArrayList<String>();
+		boolean isWindows = Platform.getOS().startsWith("win");
+
+		// Is windows?
+		if (isWindows) { // $NON-NLS-1$
+			// Handle command in a different way
+			cmdLine.add("cmd"); //$NON-NLS-1$
+			cmdLine.add("/c"); //$NON-NLS-1$
+			cmdLine.add(WHERE_COMMAND);
+		} else {
+			// Assume all other OS behave as *nix systems.
+			cmdLine.add(WHICH_LOCATION_1);
+		}
+
+		cmdLine.add(commandName);
+
+		String output = executeCmd(cmdLine);
+
+		if (!isWindows) {
+			if (output.equals(EMPTY)) {
+				cmdLine.remove(0);
+				cmdLine.add(0, WHICH_LOCATION_2);
+				output = executeCmd(cmdLine);
+				if (output.equals(EMPTY)) {
+					cmdLine.remove(0);
+					cmdLine.add(0, WHICH_LOCATION_3);
+					output = executeCmd(cmdLine);
+				}
+			}
+		}
+
+		return output;
+	}
+
 	/**
 	 * Executes a command on the CLI and return the execution output.
-	 * 
+	 *
 	 * Will return an empty String if command failed to execute.
 	 */
 	private static String executeCmd(List<String> commands) {
