e5267c638f6f80e1add0c7da45dccda9bbf060e5
Victor Rubezhny <vrubezhny@redhat.com>
2016-10-07 02:20:37 +0200

    Bug 505317 - IllegalArgumentException in JavaSearchQuery.<init>
    Fixed FindDeclarationsInWorkingSetAction.createQuery(IJavaScriptElement) - it should never return null.
    
    Signed-off-by: Victor Rubezhny <vrubezhny@redhat.com>

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/ui/actions/FindDeclarationsInWorkingSetAction.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/ui/actions/FindDeclarationsInWorkingSetAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -100,8 +100,9 @@ public class FindDeclarationsInWorkingSetAction extends FindDeclarationsAction {
 		IWorkingSet[] workingSets= fWorkingSet;
 		if (fWorkingSet == null) {
 			workingSets= factory.queryWorkingSets();
-			if (workingSets == null)
-				return null;
+			if (workingSets == null) {
+				return super.createQuery(element);
+			}
 		}
 		SearchUtil.updateLRUWorkingSets(workingSets);
 		IJavaScriptSearchScope scope= factory.createJavaSearchScope(workingSets, true);
