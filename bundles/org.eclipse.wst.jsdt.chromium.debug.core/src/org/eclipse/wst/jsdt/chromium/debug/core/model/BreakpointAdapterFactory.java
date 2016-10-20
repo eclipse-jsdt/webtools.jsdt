d510d50d426d857df6c65ccff4a5edc5f9d87bb1
Richard Kureš <rkures@tdp.cz>
2016-10-20 15:51:20 +0200

    Bug 496914 - Nullpointer in JavaScript Development Tools Chromium/V8 Remote Debugger: BreakpointAdapterFactory
    getAdapter() method throws NPE in ResourceBundlePlugin. 
    I have added null check on editorInput. It is probably safe to return
    null if there is no editorInput.
    
    
    Signed-off-by: Richard Kureš <rkures@tdp.cz>

--- bundles/org.eclipse.wst.jsdt.chromium.debug.core/src/org/eclipse/wst/jsdt/chromium/debug/core/model/BreakpointAdapterFactory.java
+++ bundles/org.eclipse.wst.jsdt.chromium.debug.core/src/org/eclipse/wst/jsdt/chromium/debug/core/model/BreakpointAdapterFactory.java
@@ -1,38 +1,45 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009, 2016 The Chromium Authors. All rights reserved.
 // This program and the accompanying materials are made available
 // under the terms of the Eclipse Public License v1.0 which accompanies
 // this distribution, and is available at
 // http://www.eclipse.org/legal/epl-v10.html
+//
+// Contributors:
+//     eb6dd07e13178c83c12323e16a84554108d40333 - Bug 496914 Nullpointer in JavaScript Development Tools Chromium/V8 Remote Debugger: BreakpointAdapterFactory
 
 package org.eclipse.wst.jsdt.chromium.debug.core.model;
 
-import org.eclipse.wst.jsdt.chromium.debug.core.util.ChromiumDebugPluginUtil;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.debug.ui.actions.IToggleBreakpointsTarget;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.wst.jsdt.chromium.debug.core.util.ChromiumDebugPluginUtil;
 
 /**
  * Factory of LineBreakpointAdapters for browser scripts.
  */
 public class BreakpointAdapterFactory implements IAdapterFactory {
-
+  
+  @Override
   @SuppressWarnings("unchecked")
   public Object getAdapter(Object adaptableObject, Class adapterType) {
     if (adaptableObject instanceof ITextEditor) {
       ITextEditor editorPart = (ITextEditor) adaptableObject;
-      IResource resource =
-          (IResource) editorPart.getEditorInput().getAdapter(IResource.class);
-      if (resource != null) {
-        String extension = resource.getFileExtension();
-        if (extension != null && ChromiumDebugPluginUtil.SUPPORTED_EXTENSIONS.contains(extension)) {
-          return new LineBreakpointAdapter.ForVirtualProject();
+      IEditorInput editorInput = editorPart.getEditorInput();
+      if (editorInput != null) {
+        IResource resource = editorInput.getAdapter(IResource.class);
+        if (resource != null) {
+          String extension = resource.getFileExtension();
+          if (extension != null && ChromiumDebugPluginUtil.SUPPORTED_EXTENSIONS.contains(extension)) {
+            return new LineBreakpointAdapter.ForVirtualProject();
+          }
         }
       }
     }
     return null;
   }
-
+  
   @SuppressWarnings("unchecked")
   public Class[] getAdapterList() {
     return new Class[] { IToggleBreakpointsTarget.class };
