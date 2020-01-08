87e1f5c2c0c6e44b1a18793f852aa660816f3069
Nitin Dahyabhai <nitind@us.ibm.com>
2020-01-08 13:27:50 -0500

    [558902] Compilation failure in 3.17m1 instantiating StreamProxy

--- bundles/org.eclipse.wst.jsdt.debug.rhino.ui/src/org/eclipse/wst/jsdt/debug/internal/rhino/ui/launching/RhinoProcess.java
+++ bundles/org.eclipse.wst.jsdt.debug.rhino.ui/src/org/eclipse/wst/jsdt/debug/internal/rhino/ui/launching/RhinoProcess.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.wst.jsdt.debug.internal.rhino.ui.launching;
 
+import java.nio.charset.Charset;
+
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
@@ -17,6 +19,7 @@ import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.core.model.IStreamsProxy;
 import org.eclipse.debug.internal.core.StreamsProxy;
 import org.eclipse.wst.jsdt.debug.internal.core.launching.JavaScriptProcess;
+import org.eclipse.wst.jsdt.debug.internal.rhino.ui.RhinoUIPlugin;
 import org.eclipse.wst.jsdt.debug.rhino.debugger.RhinoDebugger;
 
 /**
@@ -45,11 +48,30 @@ public class RhinoProcess extends JavaScriptProcess {
 		String capture = launch.getAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT);
 		boolean needsstreams = capture == null ? true : Boolean.valueOf(capture).booleanValue();
 		if(needsstreams) {
-			streamproxy = new StreamsProxy(p, launch.getAttribute(DebugPlugin.ATTR_CONSOLE_ENCODING));
+			streamproxy = new StreamsProxy(p, Charset.forName(launch.getAttribute(DebugPlugin.ATTR_CONSOLE_ENCODING)));
 		}
 		setAttribute(IProcess.ATTR_PROCESS_TYPE, TYPE);
 	}
 	
+	/**
+	 * Creates and returns the streams proxy associated with this process.
+	 *
+	 * @return streams proxy - based on org.eclipse.debug.core.model.RuntimeProcess.createStreamsProxy()
+	 */
+	protected IStreamsProxy createStreamsProxy() {
+		String encoding = getLaunch().getAttribute(DebugPlugin.ATTR_CONSOLE_ENCODING);
+		Charset charset = null;
+		if (encoding != null) {
+			try {
+				charset = Charset.forName(encoding);
+			} catch (Exception e) {
+				RhinoUIPlugin.log(e);
+				charset = Charset.defaultCharset();
+			}
+		}
+		return new StreamsProxy(process, charset);
+	}
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.wst.jsdt.debug.internal.core.launching.JavaScriptProcess#terminate()
 	 */
