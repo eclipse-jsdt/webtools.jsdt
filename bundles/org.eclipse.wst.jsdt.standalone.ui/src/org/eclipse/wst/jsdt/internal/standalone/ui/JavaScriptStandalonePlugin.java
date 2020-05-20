fc64d906eca1f4a3c878480532e60e1a6d99eab8
Victor Rubezhny <vrubezhny@redhat.com>
2020-05-20 13:25:55 +0200

    Bug 563395 - Remove standalone JavaScript editors from Web UI
    Change-Id: I49bdc4d5d7fefb0f5848bca4288d200f56660b5c
    Signed-off-by: Victor Rubezhny <vrubezhny@redhat.com>

---
+++ bundles/org.eclipse.wst.jsdt.standalone.ui/src/org/eclipse/wst/jsdt/internal/standalone/ui/JavaScriptStandalonePlugin.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2020 RedHat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ *     RedHat, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.jsdt.internal.standalone.ui;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class JavaScriptStandalonePlugin extends AbstractUIPlugin {
+
+	public JavaScriptStandalonePlugin() {
+	}
+
+}
