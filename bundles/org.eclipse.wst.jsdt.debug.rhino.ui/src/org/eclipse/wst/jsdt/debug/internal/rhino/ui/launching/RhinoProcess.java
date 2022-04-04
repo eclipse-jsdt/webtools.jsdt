3fed3d875094dbcefceb856cbaaba608dff01a06
Nitin Dahyabhai <thatnitind@gmail.com>
2022-04-04 19:43:44 -0400

    [nobug] Update for debug core internal changes

--- bundles/org.eclipse.wst.jsdt.debug.rhino.ui/src/org/eclipse/wst/jsdt/debug/internal/rhino/ui/launching/RhinoProcess.java
+++ bundles/org.eclipse.wst.jsdt.debug.rhino.ui/src/org/eclipse/wst/jsdt/debug/internal/rhino/ui/launching/RhinoProcess.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2020 IBM Corporation and others.
+ * Copyright (c) 2010, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -48,7 +48,7 @@ public class RhinoProcess extends JavaScriptProcess {
 		String capture = launch.getAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT);
 		boolean needsstreams = capture == null ? true : Boolean.valueOf(capture).booleanValue();
 		if(needsstreams) {
-			streamproxy = new StreamsProxy(p, Charset.forName(launch.getAttribute(DebugPlugin.ATTR_CONSOLE_ENCODING)));
+			streamproxy = new StreamsProxy(p, Charset.forName(launch.getAttribute(DebugPlugin.ATTR_CONSOLE_ENCODING)), "js"); //$NON-NLS-1$
 		}
 		setAttribute(IProcess.ATTR_PROCESS_TYPE, TYPE);
 	}
@@ -69,7 +69,7 @@ public class RhinoProcess extends JavaScriptProcess {
 				charset = Charset.defaultCharset();
 			}
 		}
-		return new StreamsProxy(process, charset);
+		return new StreamsProxy(process, charset, "js"); //$NON-NLS-1$
 	}
 
 	/* (non-Javadoc)
